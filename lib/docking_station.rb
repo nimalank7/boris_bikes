255722ccfe2fb1d765346c40443307a9965cda09
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-10-30 16:52:41 +0000

    refactored release_bike and dock_bike to respect SRP

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,15 +9,18 @@ class DockingStation
     available? ? get_bike : (raise Exception.new("No bikes"))
   end
   def dock_bike(bike)
-    @bikes.size < 20 ? @bikes << bike : (raise Exception.new("Dock is full"))
+    full? ? @bikes << bike : (raise Exception.new("Dock is full"))
   end
 
   def available?
-    @bikes.empty? ? false : true
+    !@bikes.empty?
   end
 
   private
   def get_bike
     @bikes.pop
+  end
+  def full?
+    @bikes.size < 20
   end
 end
